c5d03f89f5b1f84cda5159872129832cef677dcd
awashington <awashington@healthqlix.com>
2022-08-02 10:31:40 -0400

    init

---
+++ src/components/Links.vue
@@ -0,0 +1,30 @@
+<template>
+<p  class="text-3xl font-medium p-4">
+    Social Links  
+</p>
+<ul>
+    <li>
+        Instagram 
+    </li>
+    <li>
+        TikTok
+    </li>
+    <li>
+        Youtube
+    </li>
+    <li>
+        Twitch
+    </li>
+    <li>
+        Twitter
+    </li>
+</ul>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
