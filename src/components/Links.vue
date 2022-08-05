a4b68495520b40122d95f0c2aacb6c4336285f57
Life75 <austynwashington@yahoo.com>
2022-08-05 13:26:45 -0400

    links finished for now

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex-row">
 
-        <p class="text-3xl font-medium p-4">@Auzzydq</p>
+        <p @click="directToTwitch" class="text-3xl font-medium p-4 cursor-pointer">@Auzzydq</p>
         <!--Src image here -->
         <p class="font-light p-4">Hey guys! Here you can find links to my other platforms.</p>
         <div class="flex flex-row  justify-center items-center">
@@ -32,7 +32,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="links-tag">
+                            <a href="https://www.twitch.tv/auzzydq" class="links-tag">
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30"
                                     viewBox="0 0 30 30" style=" fill:#6a0dad;">
                                     <path
@@ -43,7 +43,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="links-tag">
+                            <a href="https://twitter.com/Rap_Profit" class="links-tag">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     style="fill:#1DA1F2;">
                                     <path
@@ -53,7 +53,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="links-tag">
+                            <a href="https://soundcloud.com/syn-lofi" class="links-tag">
                                 <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="24"
                                     viewBox="0 0 48 48" style=" fill:#000000;">
                                     <linearGradient id="Ke5cRwe9ua_Q0F8Xn-Y0ua_fL0lnkfCeZWT_gr1" x1="24.083" x2="24.083"
@@ -67,14 +67,14 @@
                                 </svg> <span class="flex-1 ml-3 whitespace-nowrap">Soundcloud</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="#" class="links-tag">
+                        <li v-show="false">
+                            <a  href="#" class="links-tag">
                                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" style="fill:#1DB954
 " fill-rule="evenodd" clip-rule="evenodd"><path d="M19.098 10.638c-3.868-2.297-10.248-2.508-13.941-1.387-.593.18-1.22-.155-1.399-.748-.18-.593.154-1.22.748-1.4 4.239-1.287 11.285-1.038 15.738 1.605.533.317.708 1.005.392 1.538-.316.533-1.005.709-1.538.392zm-.126 3.403c-.272.44-.847.578-1.287.308-3.225-1.982-8.142-2.557-11.958-1.399-.494.15-1.017-.129-1.167-.623-.149-.495.13-1.016.624-1.167 4.358-1.322 9.776-.682 13.48 1.595.44.27.578.847.308 1.286zm-1.469 3.267c-.215.354-.676.465-1.028.249-2.818-1.722-6.365-2.111-10.542-1.157-.402.092-.803-.16-.895-.562-.092-.403.159-.804.562-.896 4.571-1.045 8.492-.595 11.655 1.338.353.215.464.676.248 1.028zm-5.503-17.308c-6.627 0-12 5.373-12 12 0 6.628 5.373 12 12 12 6.628 0 12-5.372 12-12 0-6.627-5.372-12-12-12z"/></svg><span class="flex-1 ml-3 whitespace-nowrap">Spotify</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="links-tag">
+                            <a href="https://github.com/Life75" class="links-tag">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg><span class="flex-1 ml-3 whitespace-nowrap">Github</span>
                             </a>
                         </li>
@@ -86,6 +86,12 @@
 </template>
 
 <script lang="ts" setup>
+
+
+function directToTwitch() {
+    window.location.href = 'https://www.twitch.tv/auzzydq';
+}
+
 
 </script>
 
